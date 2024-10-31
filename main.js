6a63059f81c4e366fcb06e803687aa162c05b745
Andreslogi <andreslopez010@hotmail.com>
2024-10-31 17:25:38 -0500

    cambio de repositorio

---
+++ main.js
@@ -0,0 +1,16 @@
+// Función para mostrar las propiedades según el botón clicado (venta o arriendo)
+
+/*
+function mostrarPropiedades(tipo) {
+    const seccionVenta = document.getElementById('propiedadesVenta');
+    const seccionArriendo = document.getElementById('arriendo');
+
+    if (tipo === 'venta') {
+        seccionVenta.classList.remove('oculto');
+        seccionArriendo.classList.add('oculto');
+    } else if (tipo === 'arriendo') {
+        seccionVenta.classList.add('oculto');
+        seccionArriendo.classList.remove('oculto');
+    }
+}
+*/
